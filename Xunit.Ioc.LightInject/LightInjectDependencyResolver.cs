366e550fc4cfdaa541f9e308285490aa882e246d
Vegar Vikan <vegar@vi-kan.net>
2014-07-10 18:49:37 +0200

    Implemented IDependencyResolver for LightInject

---
+++ Xunit.Ioc.LightInject/LightInjectDependencyResolver.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using LightInject;
+
+namespace Xunit.Ioc.LightInject
+{
+    public class LightInjectDependencyResolver : IDependencyResolver
+    {
+        private readonly ServiceContainer _container;
+
+        public LightInjectDependencyResolver(ServiceContainer container)
+        {
+            _container = container;
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public object GetType(Type type)
+        {
+            return _container.GetInstance(type);
+        }
+
+        public IDependencyScope CreateScope()
+        {
+            _container.BeginScope();
+            return this;
+        }
+    }
+}
---
+++ Xunit.Ioc.LightInject/LightInjectTestCompositeRoot.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Reflection;
+using LightInject;
+
+namespace Xunit.Ioc.LightInject
+{
+    public static class LightInjectTestModule
+    {
+        public static void RegisterTestModules(this IServiceRegistry serviceRegistry, Assembly assembly)
+        {
+            var testClasses = from type in assembly.GetTypes()
+                              where type.IsClass && type.IsAbstract == false && type.IsGenericTypeDefinition == false
+                              let runWithAttr = type.GetCustomAttributes(typeof(RunWithAttribute), false)
+                                  .Cast<RunWithAttribute>()
+                                  .FirstOrDefault()
+                              where runWithAttr != null && runWithAttr.TestClassCommand == typeof(IocTestClassCommand)
+                              select type;
+
+
+            foreach (var testClass in testClasses)
+            {
+                serviceRegistry.Register(testClass, new PerScopeLifetime());
+            }
+        }
+    }
+}
